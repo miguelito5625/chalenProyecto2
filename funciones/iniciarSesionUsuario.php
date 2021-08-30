55680feaf88d7bfc8a031941885f214760bc68a4
exbinario <miguel.4to.1994@gmail.com>
2021-08-29 20:46:59 -0600

    ya se redirecciona hacia sus paginas principales segun el rol que esta logueanddo

---
+++ funciones/iniciarSesionUsuario.php
@@ -0,0 +1,30 @@
+<?php
+header('Content-Type: application/json');
+
+include 'conexion.php';
+
+$cui = $_POST["cui"];
+$clave = hash('md5', $_POST["clave"]);
+
+// echo "clave enviada: $clave \n";
+
+
+$sql = "SELECT id, cui, nombres, apellidos, nacimiento, idRol, clave FROM usuario  WHERE cui = '$cui' AND clave = '$clave' AND idRol = 2";
+$result = $conn->query($sql);
+$rows = array();
+$conn->close();
+if ($result->num_rows == 1) {
+
+  while ($row = $result->fetch_assoc()) {
+    $rows[] = $row;
+  }
+  $myObj->usuario = $rows;
+  $myObj->mensaje = "usuario existe";
+  $myObj->estado = "ok";
+  print json_encode($myObj);
+} else {
+  $myObj->usuario = $rows;
+  $myObj->mensaje = "usuario no existe";
+  $myObj->estado = "error";
+  print json_encode($myObj);
+}
---
+++ paginas/loginv2/funcionesLogin.js
@@ -0,0 +1,149 @@
+function registroUsuario() {
+    const usuario_cui = document.getElementById('usuario_cui').value;
+    const usuario_nombres = document.getElementById('usuario_nombres').value;
+    const usuario_apellidos = document.getElementById('usuario_apellidos').value;
+    const usuario_nacimiento = document.getElementById('usuario_nacimiento').value;
+    const usuario_clave = document.getElementById('usuario_clave').value;
+
+
+    const datosUsuario = {
+        cui: usuario_cui,
+        nombres: usuario_nombres,
+        apellidos: usuario_apellidos,
+        nacimiento: usuario_nacimiento,
+        clave: usuario_clave
+    };
+
+    console.log(datosUsuario);
+
+    $.ajax({
+        url: "/desarrollo/proyecto2/funciones/guardarUsuario.php",
+        type: "post",
+        data: datosUsuario,
+        success: function (response) {
+
+            console.log(response);
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        }
+    });
+}
+
+function inicioSesionAdmin() {
+    const cui_admin = document.getElementById('cui_admin').value;
+    const clave_admin = document.getElementById('clave_admin').value;
+
+    const datosAdmin = {
+        cui: cui_admin,
+        clave: clave_admin
+    };
+
+    // console.log(datosAdmin);
+
+    swalCargando();
+
+    $.ajax({
+        url: "/desarrollo/proyecto2/funciones/iniciarSesionAdmin.php",
+        type: "post",
+        data: datosAdmin,
+        success: function (response) {
+            console.log(response);
+
+            if (response.estado === "ok") {
+                setTimeout(() => {
+                    Swal.fire(
+                        'Sesion iniciada!',
+                        'Click en el boton para continuar',
+                        'success'
+                    ).then((result) => {
+                        //Redireccionar aqui
+                        window.location.href = "/desarrollo/proyecto2/paginas/sistema/administrador/principal.html";
+                    });
+                }, 2000);
+            } else {
+                setTimeout(() => {
+                    Swal.fire(
+                        'Usuario no existe',
+                        '',
+                        'error'
+                    ).then((result) => {
+                        //Aqui de momento no hacer nada
+                    });
+                }, 2000);
+            }
+
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        }
+    });
+}
+
+
+function inicioSesionUsuario() {
+    const cui_usuario = document.getElementById('cui_usuario').value;
+    const clave_usuario = document.getElementById('clave_usuario').value;
+
+    const datosUsuario = {
+        cui: cui_usuario,
+        clave: clave_usuario
+    };
+
+    // console.log(datosUsuario);
+
+    swalCargando();
+
+    $.ajax({
+        url: "/desarrollo/proyecto2/funciones/iniciarSesionUsuario.php",
+        type: "post",
+        data: datosUsuario,
+        success: function (response) {
+            console.log(response);
+
+            if (response.estado === "ok") {
+                setTimeout(() => {
+                    Swal.fire(
+                        'Sesion iniciada!',
+                        'Click en el boton para continuar',
+                        'success'
+                    ).then((result) => {
+                        //Redireccionar aqui
+                        window.location.href = "/desarrollo/proyecto2/paginas/sistema/usuario/principal.html";
+                    });
+                }, 2000);
+            } else {
+                setTimeout(() => {
+                    Swal.fire(
+                        'Usuario no existe',
+                        '',
+                        'error'
+                    ).then((result) => {
+                        //Aqui de momento no hacer nada
+                    });
+                }, 2000);
+            }
+
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        }
+    });
+}
+
+
+function swalCargando() {
+    Swal.fire({
+        title: 'Iniciando sesion',
+        timerProgressBar: true,
+        // timer: 2000,
+        didOpen: () => {
+            Swal.showLoading()
+        }
+    }).then((result) => {
+
+    })
+}
